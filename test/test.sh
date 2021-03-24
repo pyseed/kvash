73f1d504f9baf6638b83a3cd740cb87ac19b3fbf
pyseed <pyseed.dev@gmail.com>
2021-03-24 20:10:45 +0100

    [ADD] test tools moved to libash repo

--- test/test.sh
+++ test/test.sh
@@ -2,12 +2,6 @@
 export SHKV_STORE="${HOME}/tmp/shkv"
 #verbose=true
 
-# onBeforeSuite () {
-# }
-
-# onAfterSuite () {
-# }
-
 onBeforeIt () {
     rm "${SHKV_STORE}/$1" 2> /dev/null
 }
@@ -16,8 +10,8 @@ onAfterIt () {
     rm "${SHKV_STORE}/$1" 2> /dev/null
 }
 
-. lib.sh
-
+curl https://raw.githubusercontent.com/pyseed/libash/master/test.sh > libash_test.sh
+. ./libash_test.sh
 
 # force key value
 # setKey key value
@@ -29,24 +23,19 @@ setKey () {
     echo  -n "${value}" > "${filePath}"
 }
 
-# test key value, compare with file
-# testKey expectedContentFilePath
-testKeyFile () {
+# assertKeyFile expectedContentFilePath
+assertKeyFile () {
     local expectedContentFilePath="$1"
     local filePath="${SHKV_STORE}/${current}"
 
-    cmpFile "${filePath}" "${expectedContentFilePath}"
+    assertFile "${filePath}" "${expectedContentFilePath}"
 }
 
-# test key value, compare witj value
-# testKeyValue expectedValue
-testKeyValue () {
+# assertKeyValue expectedValue
+assertKeyValue () {
     local filePath="${SHKV_STORE}/${current}"
-    local tmpFile=$(getTmpFilePath)
 
-    echo  -n "$1" > "${tmpFile}"
-    cmpFile "${filePath}" "${tmpFile}"
-    rm "${tmpFile}"
+    assertFileContent "${filePath}" "${testKeyValue}"
 }
 
 
@@ -63,29 +52,29 @@ suiteGeneral () {
     # set1
     it set1
     ../shkv set "${current}" world
-    testKeyValue world
+    assertKeyValue world
 
     # set2
     it set2
     ../shkv set "${current}" "world test"
-    testKeyValue "world test"
+    assertKeyValue "world test"
 
     # get
     it get
     setKey "${current}" world
     result=$(../shkv get "${current}")
-    cmpResult "${result}" world
+    assertResult "${result}" world
 
     # path
     it path
     result=$(../shkv path "${current}")
-    cmpResult "${result}" "${SHKV_STORE}/${current}"
+    assertResult "${result}" "${SHKV_STORE}/${current}"
 
     # del
     it del
     setKey "${current}" world
     ../shkv del "${current}"
-    [ -f "${SHKV_STORE}/${current}" ] && logFail || logSuccess
+    assertIsNotFile "${SHKV_STORE}/${current}"
 }
 
 suiteAppend () {
@@ -94,24 +83,24 @@ suiteAppend () {
     # append from empty
     it append_from_empty
     ../shkv append "${current}" "append entry"
-    testKeyValue "append entry"
+    assertKeyValue "append entry"
 
     # append
     it append
     setKey "${current}" world
     ../shkv "${current}" append "append entry"
-    testKeyValue "worldappend entry"
+    assertKeyValue "worldappend entry"
 
     # appendr from empty
     it appendr_from_empty
     ../shkv appendr "${current}" "appendr entry"
-    testKeyFile ./check/appendrFromEmpty.txt
+    assertKeyFile ./check/appendrFromEmpty.txt
 
     # appendr
     it appendr
     setKey "${current}" world
     ../shkv appendr "${current}" "appendr entry"
-    testKeyFile ./check/appendr.txt
+    assertKeyFile ./check/appendr.txt
 }
 
 suiteList () {
@@ -120,27 +109,27 @@ suiteList () {
     # add1
     it list_add1
     ../shkv list add "${current}" item1
-    testKeyFile ./check/listAdd1.txt
+    assertKeyFile ./check/listAdd1.txt
 
     # add2
     it list_add2
     setKey "${current}" item1
     echo "" >> "${SHKV_STORE}/${current}"
     ../shkv list add "${current}" item2
-    testKeyFile ./check/listAdd2.txt
+    assertKeyFile ./check/listAdd2.txt
 
     # del
     it list_del
     cat ./dataset/listDel.txt > "${SHKV_STORE}/${current}"
     ../shkv list del "${current}" item2
-    testKeyFile ./check/listDel.txt
+    assertKeyFile ./check/listDel.txt
     # item2item2 should not be destroyed by item2 del
 
     # foreach
     it list_foreach
     cat ./dataset/listForeach.txt > "${SHKV_STORE}/${current}"
     ../shkv list foreach "${current}" listForeachCallback
-    cmpFile /tmp/shkv_foreach.txt ./check/foreach.txt
+    assertFile /tmp/shkv_foreach.txt ./check/foreach.txt
     rm /tmp/shkv_foreach.txt 2> /dev/null
 }
 
@@ -151,25 +140,25 @@ suiteDict () {
     it dict_set
     ../shkv dict set "${current}" one oneword "comment of one"
     ../shkv dict set "${current}" two twoword "comment of two"
-    testKeyFile ./check/dictSet.txt
+    assertKeyFile ./check/dictSet.txt
 
     # get
     it dict_get
     cat ./dataset/dict.txt > "${SHKV_STORE}/${current}"
     result=$(../shkv dict get "${current}" two)
-    cmpResult "${result}" twoword
+    assertResult "${result}" twoword
 
     # props
     it dict_props
     cat ./dataset/dict.txt > "${SHKV_STORE}/${current}"
     result=$(../shkv dict props "${current}")
-    cmpResult "${result}" "one=oneword two=twoword twotwo=twotwoword three=threeword"
+    assertResult "${result}" "one=oneword two=twoword twotwo=twotwoword three=threeword"
 
     # del
     it dict_del
     cat ./dataset/dict.txt > "${SHKV_STORE}/${current}"
     ../shkv dict del "${current}" two
-    testKeyFile ./check/dictDel.txt
+    assertKeyFile ./check/dictDel.txt
     # twotwo=twotwoword should not be destroyed by two del
 }
 
